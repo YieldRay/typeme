4aaa83f5cfb7c12370dd75976320f56c7ac8f1eb
YieldRay <yieldray@outlook.com>
2024-04-04 22:17:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# typeme
+
+Attach typing effect to any `Node`! (Based on [`Node.textContent`](https://developer.mozilla.org/docs/Web/API/Node/textContent))
+
+```ts
+import withNode from "./dist/index.mjs";
+
+const stop = withNode(document.querySelect("#target"))
+    .wait(500)
+    .text("Hello...")
+    .wait(1000)
+    .text("Hello, world!")
+    .wait(500)
+    .text("你好，世界！")
+    .repeat()
+    .start();
+```
---
+++ src/index.ts
@@ -0,0 +1,94 @@
+export const sleep = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms));
+
+interface Chain {
+    text: (text: string) => Chain;
+    wait: (ms: number) => Chain;
+    /**
+     * @param [n=Infinity] by default `Infinity`
+     */
+    repeat: (n?: number) => Chain;
+    /**
+     * await for any callback
+     */
+    then: (callback: () => unknown | Promise<unknown>) => Chain;
+    /**
+     * start type effect, returns a function to stop
+     */
+    start: () => VoidFunction;
+}
+
+export default function withNode(node: Node) {
+    const ops: Array<() => unknown | Promise<unknown>> = [];
+    let isStopped = false;
+    let repeatTimes = 1;
+
+    const start = () => {
+        requestAnimationFrame(async () => {
+            const loop = async () => {
+                if (--repeatTimes < 0) return;
+                for (const op of ops) if (!isStopped) await op();
+                if (!isStopped) if (repeatTimes > 0) await loop();
+            };
+            await loop();
+        });
+        const stop = () => {
+            isStopped = true;
+        };
+        return stop;
+    };
+
+    const chain: Chain = {
+        text: (text: string) => {
+            ops.push(async () => {
+                await edit(node, text);
+            });
+            return chain;
+        },
+        wait: (ms: number) => {
+            ops.push(() => sleep(ms));
+            return chain;
+        },
+        then: (callback: () => unknown | Promise<unknown>) => {
+            ops.push(callback);
+            return chain;
+        },
+        repeat: (n = Infinity) => {
+            repeatTimes = n;
+            return chain;
+        },
+        start,
+    };
+    return chain;
+}
+
+/**
+ * Based on camwiegert's [typical](https://github.com/camwiegert/typical) library
+ *
+ * MIT LICENSE
+ */
+export async function edit(node: Node, next: string, speed = 60) {
+    const curr = node.textContent || "";
+    const overlap = getOverlap(curr, next);
+
+    for (const textContent of [...deleter(curr, overlap), ...writer(next, overlap)]) {
+        requestAnimationFrame(() => (node.textContent = textContent)); // this is unstoppable for now
+        await sleep(speed + speed * (Math.random() - 0.5)); // sleep for single char type
+    }
+}
+
+export function* writer([...text]: string, startIndex = 0, endIndex = text.length) {
+    while (startIndex < endIndex) {
+        yield text.slice(0, ++startIndex).join("");
+    }
+}
+
+export function* deleter([...text]: string, startIndex = 0, endIndex = text.length) {
+    while (endIndex > startIndex) {
+        yield text.slice(0, --endIndex).join("");
+    }
+}
+
+export function getOverlap([...start]: string, [...end]: string) {
+    start.push("\0");
+    return start.findIndex((char, i) => end[i] !== char);
+}
